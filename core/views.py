ff603eb17bd3cb362b2cefd5f2f328b696adabab
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2026-05-10 19:13:49 +0200

    feat: Implementación de plantilla base, Navbar superior, menú lateral Offcanvas y estilos corporativos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def home_view(request):
+    """Renderiza la plantilla base para comprobar la estructura de navegación"""
+    return render(request, 'base.html')
