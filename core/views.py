4cd51cb59fffa646b699f723477591220ab4738d
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2026-05-25 10:14:27 +0200

    feat: interfaz de asistencia dinámica con fetch api y delegación de eventos JS

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Group, Beneficiary
 
 def home_view(request):
@@ -19,7 +20,27 @@ def attendance_view(request):
         'groups': groups,
         'students': students,
     }
- 
-    
     # Pasamos los datos al HTML mediante el "contexto" (el diccionario final)
     return render(request, 'attendance.html', context)
+
+def get_students_by_group(request, group_id):
+    """API interna: DEvuelve los alumnos de un grupo específico en formato JSON"""
+    try:
+        group = Group.objects.get(id=group_id)
+        # Obtenemos los y las alumnas ordenadas alfabéticamente por el primer apellido
+        students = group.beneficiaries.all().order_by('last_name1')
+
+        # Formateamos los datos en una lista de diccionarios
+        students_data = []
+        for s in students:
+            # Construimos el nombre completo con el segundo apellido (en caso de que exista).
+            full_name = f"{s.first_name} {s.last_name1} {s.last_name2 or ''}".strip()
+            students_data.append({
+                'id': s.id,
+                'full_name': full_name,
+            })
+        return JsonResponse({'students': students_data})
+    
+    except Group.DoesNotExist:
+            # En caso de que no se encuentre el grupo, devolvemos un mensaje de error
+        return JsonResponse({'error': 'Grupo no encontrado'}, status=404)
